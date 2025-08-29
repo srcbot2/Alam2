0719c453255de9b51772840974fc96c43235e7dc
srcbot2 <komalrani2m+1@gmail.com>
2025-08-29 09:40:25 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21240850"))
-API_HASH = environ.get("API_HASH", "593eec13b2638de5e9bb4284ea8631ec")
-BOT_TOKEN = environ.get("BOT_TOKEN", "s8427961753:AAEfQ5LSfcfuM4IlDzwfI6a4qJOV6yXoMaA")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "6662470778"))
-CREDIT = environ.get("CREDIT", "💞𝗔𝗹𝗲𝘅 𝗔𝗿𝘆𝗮💞")
+OWNER = int(environ.get("OWNER", "5680454765"))
+CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -20,7 +20,3 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
-
-
-
-
